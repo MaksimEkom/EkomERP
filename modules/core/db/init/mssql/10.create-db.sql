08d284704eb0695eaa7c89c5cb53330676376823
ZakkHarov <sa.zakharov@ekom.by>
2017-06-19 12:46:39 +0300

    1

--- modules/core/db/init/mssql/10.create-db.sql
+++ modules/core/db/init/mssql/10.create-db.sql
@@ -451,3 +451,26 @@ create table EKOMERP_PRODUCT_PRICE (
     primary key nonclustered (ID)
 )^
 -- end EKOMERP_PRODUCT_PRICE
+-- begin EKOMERP_CALCULATION
+create table EKOMERP_CALCULATION (
+    ID uniqueidentifier,
+    VERSION integer not null,
+    CREATE_TS datetime2,
+    CREATED_BY varchar(50),
+    UPDATE_TS datetime2,
+    UPDATED_BY varchar(50),
+    DELETE_TS datetime2,
+    DELETED_BY varchar(50),
+    --
+    NUMBER_ varchar(255) not null,
+    NAME varchar(255) not null,
+    PRODUCT_ID uniqueidentifier,
+    SELLING_PRICE_TOTAL double precision,
+    AMOUNT_TAX double precision,
+    SELLING_PRICE_UNTAXED double precision,
+    PROFIT double precision,
+    COST_PRICE double precision,
+    --
+    primary key nonclustered (ID)
+)^
+-- end EKOMERP_CALCULATION
---
+++ modules/core/db/update/mssql/17/170619-2-updateCalculation.sql
@@ -0,0 +1,2 @@
+alter table EKOMERP_CALCULATION drop column DATE_ ;
+alter table EKOMERP_CALCULATION alter column PRODUCT_ID uniqueidentifier ;
