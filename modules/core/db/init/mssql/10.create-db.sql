5e1c8773062bca3e901c2dfd2c1efe7ae4a5a3df
MaksimEkom <mn.zakharov@ekom.by>
2017-08-07 17:04:46 +0300

    07.08.2017
    Добавлены двери

--- modules/core/db/init/mssql/10.create-db.sql
+++ modules/core/db/init/mssql/10.create-db.sql
@@ -552,3 +552,82 @@ create table EKOMERP_PAYMENT (
     primary key nonclustered (ID)
 )^
 -- end EKOMERP_PAYMENT
+-- begin EKOMERP_DOOR
+create table EKOMERP_DOOR (
+    ID uniqueidentifier,
+    VERSION integer not null,
+    CREATE_TS datetime2,
+    CREATED_BY varchar(50),
+    UPDATE_TS datetime2,
+    UPDATED_BY varchar(50),
+    DELETE_TS datetime2,
+    DELETED_BY varchar(50),
+    --
+    DOOR_KIND integer not null,
+    CD varchar(50) not null,
+    DESIGNATION varchar(255),
+    DESCRIPTION varchar(max),
+    --
+    primary key nonclustered (ID)
+)^
+-- end EKOMERP_DOOR
+-- begin EKOMERP_DOOR_ORDER
+create table EKOMERP_DOOR_ORDER (
+    ID uniqueidentifier,
+    VERSION integer not null,
+    CREATE_TS datetime2,
+    CREATED_BY varchar(50),
+    UPDATE_TS datetime2,
+    UPDATED_BY varchar(50),
+    DELETE_TS datetime2,
+    DELETED_BY varchar(50),
+    --
+    NUMBER_ varchar(20) not null,
+    DATE_ datetime2,
+    PARTNER_ID uniqueidentifier not null,
+    MANUFACTURE_DATE datetime2,
+    PLACEMENT_CATEGORY decimal(19) not null,
+    DOOR_ID uniqueidentifier not null,
+    PRICE decimal(19, 2),
+    HEIGHT decimal(19, 2),
+    WIDTH decimal(19, 2),
+    OPENING integer not null,
+    NOTES varchar(max),
+    ISOLATION_ tinyint,
+    PEEPHOLE tinyint,
+    TRIM_ tinyint,
+    DISMANTILING tinyint,
+    LOOP_NUMBER varchar(255) not null,
+    TOP_LOCK varchar(255),
+    BUTTOMLOCK varchar(255),
+    TOP_ONLAY varchar(255),
+    BUTTOM_ONLAY varchar(255),
+    HANDLE varchar(255),
+    CYLINDER varchar(255),
+    INTERNAL_DECORATION varchar(max),
+    EXTERNAL_DECORATION varchar(max),
+    STATUS integer not null,
+    MASTER integer,
+    PAYED tinyint,
+    --
+    primary key nonclustered (ID)
+)^
+-- end EKOMERP_DOOR_ORDER
+-- begin EKOMERP_GAUGING
+create table EKOMERP_GAUGING (
+    ID uniqueidentifier,
+    VERSION integer not null,
+    CREATE_TS datetime2,
+    CREATED_BY varchar(50),
+    UPDATE_TS datetime2,
+    UPDATED_BY varchar(50),
+    DELETE_TS datetime2,
+    DELETED_BY varchar(50),
+    --
+    DOOR_ORDER_NUMBER varchar(20),
+    WIDTH integer,
+    HEIGHT integer,
+    --
+    primary key nonclustered (ID)
+)^
+-- end EKOMERP_GAUGING
